0d99341ac483119551901ce036afe97e883f6e39
Monika Williams <monika.m.williams@gmail.com>
2017-12-12 10:47:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,22 +19,5 @@ class Passenger {
     store.passengers.push(this)
   }
 }
-let tripId = 0
-class Trip {
-  constructor(driver, passenger){
-    this.id = ++tripId
-    if(driver){
-      this.driverId = driver.id
-    }
-    if(passenger){
-      this.passengerId = passenger.id
-    }
-     store.trips.push(this)
-  }
-  setDriver(driver){
-   this.driverId = driver.id
- }
- setPassenger(passenger){
-   this.passengerId = passenger.id
- }
-}
+
+
