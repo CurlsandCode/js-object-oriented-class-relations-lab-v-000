ea9befbc4636fc937b7c9de010eee92acb922279
Monika Williams <monika.m.williams@gmail.com>
2017-12-12 11:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-let store = {drivers:[], passengers:[], trips:[]};
+let store = {drivers: [], passengers: [], trips: []};
+
 
 let driverId = 0;
 
 class Driver {
+
   constructor(name){
-    this.id = ++driverId;
     this.name = name;
+    this.id = ++driverId;
 
     store.drivers.push(this);
   }
-  trips(){
-     return store.trips.filter(trip => {
-       return trip.driverId === this.id;
-     })
-   }
 
-   passengers(){
-     return this.trips().map((trip) => {
-       return trip.passenger();
-     })
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id;
+    })
+  }
+
+  passengers(){
+    return this.trips().map((trip)=> {
+      return trip.passenger()
+    })
+  }
+
 }
 
 let passengerId = 0;
 
 class Passenger {
-  constructor(name){
-    this.id = ++passengerId;
-    this.name = name;
 
-    store.passengers.push(this)
+  constructor(name){
+    this.name = name;
+    this.id = ++passengerId;
+
+    store.passengers.push(this);
   }
+
   trips(){
     return store.trips.filter(trip => {
       return trip.passengerId === this.id;
@@ -40,30 +47,36 @@ class Passenger {
     return this.trips().map(trip => {
       return trip.driver();
     })
+  }
 }
 
-let tripId = 0;
+let tripId = 0
 
 class Trip {
-  constructor(driver, passenger){
-    this.id = ++tripId;
-    if(driver){
+
+  constructor(driver, passenger) {
+    if (driver) {
       this.driverId = driver.id;
     }
-    if(passenger){
+
+    if (passenger) {
       this.passengerId = passenger.id;
     }
-     store.trips.push(this)
+
+    this.id = ++tripId;
+    store.trips.push(this);
   }
 
-    driver(){
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId;
+    }.bind(this))
+  }
+
+  driver(){
     return store.drivers.find(function(driver){
       return driver.id === this.driverId;
     }.bind(this))
   }
-    passenger(){
-    return store.passengers.find(function(passenger){
-      return passsenger.id === this.passengerId;
-    }.bind(this))
-  }
+
 }
